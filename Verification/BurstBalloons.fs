57af137c5c7e32bb5dd6c3bbebad22d86c24ba5a
Russell West <russwest@microsoft.com>
2016-03-15 12:27:59 -0700

    Burst Balloons: New test (failing)

--- Verification/BurstBalloons.fs
+++ Verification/BurstBalloons.fs
@@ -6,19 +6,20 @@ open Xunit
 open Swensen.Unquote
 
 [<Theory>]
-[<InlineData( "8"             , 8   )>]
-[<InlineData( "2 8"           , 24  )>]
-[<InlineData( "8 2"           , 24  )>]
-[<InlineData( "2 2 5"         , 35  )>]
-[<InlineData( "5 2 2"         , 35  )>]
-[<InlineData( "2 6 2"         , 30  )>]
-[<InlineData( "2 5 2"         , 26  )>]
-[<InlineData( "3 1 5 8"       , 167 )>]
-[<InlineData( "3 2 1 2 3"     , 46  )>]
-[<InlineData( "3 2 5 2 3"     , 117 )>]
-[<InlineData( "1 2 3 4 5 6"   , 252 )>]
-[<InlineData( "6 5 4 3 2 1"   , 252 )>]
-[<InlineData( "2 3 2 1 2 3 2" , 70  )>]
+[<InlineData( "8"             , 8    )>]
+[<InlineData( "2 8"           , 24   )>]
+[<InlineData( "8 2"           , 24   )>]
+[<InlineData( "2 2 5"         , 35   )>]
+[<InlineData( "5 2 2"         , 35   )>]
+[<InlineData( "2 6 2"         , 30   )>]
+[<InlineData( "2 5 2"         , 26   )>]
+[<InlineData( "3 1 5 8"       , 167  )>]
+[<InlineData( "3 2 1 2 3"     , 46   )>]
+[<InlineData( "3 2 5 2 3"     , 117  )>]
+[<InlineData( "1 2 3 4 5 6"   , 252  )>]
+[<InlineData( "6 5 4 3 2 1"   , 252  )>]
+[<InlineData( "9 2 14 5 3"    , 1053 )>]
+[<InlineData( "2 3 2 1 2 3 2" , 70   )>]
 let mainline (balloons : string) expected =
     let b =
         balloons
