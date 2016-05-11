95d956988c9d9bd25d83ed2a601f6e4403b3ef0d
Russell West <russwest@microsoft.com>
2016-05-11 16:39:15 -0700

    Lexical Dedupe start

---
+++ Solutions/LexicalDedupe.fs
@@ -0,0 +1,19 @@
+﻿module LexicalDedupe // 316. Remove Duplicate Letters
+
+let lexicalRemoveDedupe letters =
+    let getCounts letters =
+        let rec loop letters i mp = 
+            match letters with
+            | l :: r ->
+                let value =
+                    match Map.tryFind l mp with
+                    | Some(v) -> i :: v
+                    | None -> [i]
+
+                loop r (i+1) (Map.add l value mp)
+            | _ -> mp
+
+        loop letters 0 Map.empty
+
+    let counts = getCounts letters
+    counts
---
+++ Verification/LexicalDedupe.fs
@@ -0,0 +1,15 @@
+﻿module Test.LexicalDedupe
+
+open LexicalDedupe
+open Test.Common
+open Xunit
+open Swensen.Unquote
+
+[<Theory>]
+[<InlineData("bcabc", "abc")>]
+[<InlineData("cbacdcbc", "acdb")>]
+let mainline (letters : string) (expected : string) =
+    let l = letters |> Seq.toList
+    let e = expected |> Seq.toList
+
+    test <@ e = lexicalRemoveDedupe l @>
